033991847503c5c1b0bf21633991bc3baaba0920
Tyler Norman <letan881@hotmail.com>
2014-04-04 21:15:19 -0400

    Initial Commit
    Enemy ships randomly pick a target, shoot at target.

---
+++ Assets/Scripts/EnemyShipScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShipScript : MonoBehaviour 
+{
+	public float MAX_ACTION_TIME = 1.0f;
+
+	private GameObject[] potentialTargets;
+	private GameObject currentTarget = null;
+
+	private float actionTime = 0;
+	private bool isActionTimerRunning = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		potentialTargets = GameObject.FindGameObjectsWithTag ("Player");
+		currentTarget = potentialTargets[Random.Range (0, potentialTargets.Length)];
+
+		isActionTimerRunning = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isActionTimerRunning)
+			UpdateActionTimer ();
+	}
+
+	private void UpdateActionTimer()
+	{
+		actionTime += Time.deltaTime;
+		if (actionTime >= MAX_ACTION_TIME) 
+		{
+			Shoot ();
+			actionTime = 0;
+		}
+	}
+
+	private void Shoot()
+	{
+		GameObject projectile = GameObject.Instantiate (Resources.Load ("Prefabs/Projectile")) as GameObject;
+		projectile.transform.position = gameObject.transform.position;
+
+		projectile.GetComponent<ProjectileScript> ().PushTowardsLocation (currentTarget.transform.position, 5.0f);
+	}
+}
---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour 
+{
+	public float ProjectileLifetime = 1.5f; //Object lifetime in seconds.
+	
+	private float velocity = 0.0f;
+	private float currentLifetime = 0.0f;
+
+	private bool isActive = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isActive) 
+		{
+			Vector3 newPos = gameObject.transform.position + (-gameObject.transform.up * velocity * Time.deltaTime);
+			gameObject.transform.position = newPos;
+
+			UpdateTimer();
+		}
+	}
+
+	public void PushTowardsLocation(Vector3 targetPos, float vel)
+	{
+		velocity = vel;
+		isActive = true;
+
+		float rotation = Mathf.Atan2 (targetPos.y - gameObject.transform.position.y, targetPos.x - gameObject.transform.position.x) * Mathf.Rad2Deg;
+
+		//Debug.Log (rotation);
+
+		gameObject.transform.Rotate (0, 0, rotation + 90);
+	}
+
+	private void UpdateTimer()
+	{
+		currentLifetime += Time.deltaTime;
+
+		if (currentLifetime >= ProjectileLifetime) 
+		{
+			isActive = false;
+			currentLifetime = 0;
+			Destroy(gameObject);
+		}
+	}
+}
